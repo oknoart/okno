9cfce21e163e0b402fa7e26face0e531b4e87698
grzzlybr <41268972+lukenm1@users.noreply.github.com>
2025-07-27 15:52:08 +0100

    Update redact.js

--- redact.js
+++ redact.js
@@ -27,6 +27,11 @@ document.addEventListener("DOMContentLoaded", () => {
     spans.forEach((span, i) => {
       setTimeout(() => {
         span.classList.add("redacted");
+
+// Apply random angle between -4deg and +4deg
+const angle = (Math.random() * 8 - 4).toFixed(2); // -4 to +4
+span.style.transform = `rotate(${angle}deg)`;
+
       }, 10000 + i * 5000); // adjust delay here
     });
   });
